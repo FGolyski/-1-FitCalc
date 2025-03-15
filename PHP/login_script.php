573b687957426df61ff582dae111829f07bf5bb6
FilipGolyski <Patrzusiek@gmail.com>
2025-03-15 18:35:07 +0100

    Added password hashing and function that changes usernames into lowercase.

--- PHP/login_script.php
+++ PHP/login_script.php
@@ -7,12 +7,15 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $username = htmlentities($username, ENT_QUOTES,'UTF-8');
 $password = htmlentities($password, ENT_QUOTES,'UTF-8');
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$sql = "SELECT * FROM users WHERE username='$username'";
 $result = $polaczenie->query($sql);
 
 if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if(password_verify($password, $row["password"])) {
     $_SESSION['username'] = $username;
     header('Location: ../index.php');
+    }
 } else {
     header('Location: ../login.php?error=1');
 }
