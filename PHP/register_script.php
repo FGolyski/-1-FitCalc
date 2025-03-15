6a4d5bd3222f29717631ce925908896c0cfd5b1e
FilipGolyski <Patrzusiek@gmail.com>
2025-03-15 18:35:07 +0100

    Added password hashing and function that changes usernames into lowercase.

--- PHP/register_script.php
+++ PHP/register_script.php
@@ -7,14 +7,16 @@ $polaczenie = new mysqli($serwer, $user, $haslo, $baza) or die('Cant connect to 
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-
+$username = htmlentities($username, ENT_QUOTES,'UTF-8');
+$password = htmlentities($password, ENT_QUOTES,'UTF-8');
+$password_hashed = password_hash($password, PASSWORD_DEFAULT);
 $exist_check = "SELECT * FROM users WHERE username='$username'";
 $check_result = $polaczenie->query($exist_check);
 
 if ($check_result->num_rows > 0) {
     header('Location: ../login.php?error_reg=1');
 } else {
-    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password_hashed')";
 
     if ($polaczenie->query($sql) === TRUE) {
         $_SESSION['username'] = $username;
